b5ce5d89f7a0845b6fe413cd74d9f0703db3e38d
GODOmegaPi <guillaume.laubscher@heig-vd.ch>
2021-11-03 16:46:37 +0100

    added multiple integration test for notes endpoint

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,19 @@
+class UTF8Validator < ActiveModel::Validator
+  def validate(record)
+    if record.present?
+      if record.title.to_s.force_encoding("UTF-8").valid_encoding? or record.body.to_s.force_encoding("UTF-8").valid_encoding?
+        record.errors.add :base, "only UTF-8 encoding allowed"
+      end
+    end
+  end
+end
+
 class Note < ApplicationRecord
+  validates :title, :body, presence: true
+
+  validates :title, length: { minimum: 1, maximum: 64 }
+  validates :body, length: { maximum: 1024 }
+
+  # Not working for some unknown reason
+  # validates_with UTF8Validator
 end
