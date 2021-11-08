463916d655732b9c208557ec89e3d38638a63f61
GODOmegaPi <47740893+GODOmegaPi@users.noreply.github.com>
2021-11-08 16:13:33 +0100

    CD and script (#1)
    * Clean unused action
    
    * Added the script for the remote setup

---
+++ scripts/script.sh
@@ -0,0 +1,17 @@
+cd backend
+source ~/.rvm/scripts/rvm
+
+rvm use 2.7.4
+
+gem install bundler
+bundle install --jobs 4 --retry 3
+export SECRET_KEY_BASE=rake secret
+ruby -e 'p ENV["SECRET_KEY_BASE"]'
+RAILS_ENV=production rake DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+
+# rails credentials:edit RAILS_ENV=production
+bin/rails db:create RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+bin/rails db:schema:load RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+
+rm -f tmp/pids/server.pid
+bin/rails server -e production -p 4000 -d
