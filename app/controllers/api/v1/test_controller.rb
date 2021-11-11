d23561fc028a6c61aaaa9e44a2db2f832ec544f4
Eric B2A <eric.b2a@pm.me>
2021-11-11 09:08:21 +0100

    nouvelle route de test

---
+++ app/controllers/api/v1/test_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::TestController < ApplicationController
+
+  def index
+    test = {
+      'test' => "Ceci est la représentation d'un test"
+    }
+    render json: test.to_json
+  end
+end
