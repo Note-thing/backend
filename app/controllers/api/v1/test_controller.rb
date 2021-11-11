0214d4d6f36ea52b4c2a571b2630c9507468ec3d
GODOmegaPi <guillaume.laubscher@heig-vd.ch>
2021-11-11 11:30:07 +0100

    merge deploy to main

--- app/controllers/api/v1/test_controller.rb
+++ app/controllers/api/v1/test_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::TestController < ApplicationController
 
   def index
     test = {
-      'test' => "Ceci est la représentation d'un test"
+      'test' => "Ceci est la représentation d'un test!"
     }
     render json: test.to_json
   end
