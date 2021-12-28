d1a73b77fe240695acf6e566aa493e3c376afeee
Eric B2A <eric.b2a@pm.me>
2021-12-28 16:12:25 +0100

    Suppressions en cascade de folders, notes et shared_notes
    #closes 36

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,7 +1,7 @@
 class Folder < ApplicationRecord
 
   belongs_to :user
-  has_many :notes
+  has_many :notes, dependent: :destroy
 
   validates :title, length: { minimum: 1, maximum: 16}
   validates :title, presence: true
--- app/models/note.rb
+++ app/models/note.rb
@@ -13,7 +13,7 @@ class Note < ApplicationRecord
   belongs_to :folder
   has_many :note_tags, dependent: :destroy
   has_many :tags, through: :note_tags
-  has_many :shared_notes
+  has_many :shared_notes, dependent: :destroy
 
   validates :title, :body, presence: true
 
