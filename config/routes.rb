e5902256eee8ae13780818da8d495b39f62ab5ce
Eric B2A <eric.b2a@pm.me>
2021-12-29 18:46:38 +0100

    Fix cors
    https://github.com/cyu/rack-cors/issues/117

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,10 @@ Rails.application.routes.draw do
       resources :shared_notes, only: [:show, :create, :destroy]
 
       post '/login', to: 'authentications#login'
+
       post '/signup', to: 'authentications#signup'
+
+      match '*path', via: [:options], to: lambda {|_| [204, { 'Content-Type' => 'text/plain' }]}
     end
   end
 end
