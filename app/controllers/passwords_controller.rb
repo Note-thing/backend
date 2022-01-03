9adedfc9b9fd54641f674f20198cc6a91b17fcaa
Eric B2A <eric.b2a@pm.me>
2022-01-03 14:44:22 +0100

    Mailer base

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -34,4 +34,17 @@ class PasswordsController < ApplicationController
       raise NotFoundError.new("Link not valid or expired. Try generating a new link.")
     end
   end
+
+  def update
+    if !params[:password].present?
+      render json: {error: 'Password not present'}, status: :unprocessable_entity
+      return
+    end
+
+    if current_user.reset_password(params[:password])
+      render json: {status: 'ok'}, status: :ok
+    else
+      render json: {errors: current_user.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
 end
