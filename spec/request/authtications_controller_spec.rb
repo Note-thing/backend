1e2cae136607de7d4222c4d377f370fcdaedaa23
Noah Fusi <noah.fusi@heig-vd.ch>
2022-01-25 10:38:25 +0100

    Added tests for signup

--- spec/request/authtications_controller_spec.rb
+++ spec/request/authtications_controller_spec.rb
@@ -2,17 +2,20 @@ require "rails_helper"
 
 RSpec.describe "folders controller", type: :request do
   before do
-    @user = User.create(email: "aaa@aa.aa", password: "123456", password_confirmation: "123456", firstname: "pierre", lastname: "donini", email_validated: true)
+    @user = User.create(email: "noahfusi@gmail.com", password: "123456", password_confirmation: "123456", firstname: "pierre", lastname: "donini", email_validated: true)
   end
 
 
-  #it 'should register a user' do
-  #  header = {'CONTENT_TYPE' => 'application/json'}
-  # hash = { email: "bbb@bb.bb", password: '123456', password_confirmation: '123456', firstname: 'noah', lastname: 'fusi' }
-  # post '/api/v1/signup', params: hash.to_json, headers: header
-  # puts response.body
-  # response.status.should == 201
-  #end
+  it 'should register a user' do
+    header = {'CONTENT_TYPE' => 'application/json'}
+    hash = { email: "bbb@bb.bb", password: '123456', password_confirmation: '123456', firstname: 'noah', lastname: 'fusi' }
+    post '/api/v1/signup', params: hash.to_json, headers: header
+    puts response.body
+    answer = JSON.parse(response.body)
+    response.status.should == 201
+    assert User.find(answer['user']['id'])
+    assert User.find(answer['user']['id']).email_validated == false
+  end
   it 'should work with correct credentials' do
     valid_credentials = { email: "aaa@aa.aa",  password: "123456"}
     post '/api/v1/signin', params: valid_credentials
